e8476aa6d90a935eb1cedef1a94c455db3b4e641
aloicerains <aloiceokoth98@gmail.com>
2021-12-01 13:20:07 +0300

    updated

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,12 +8,31 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int lef_t = 0;
+	int righ_t = 0;
+
+	if (tree == NULL)
+		return (0);
+	lef_t += height(tree->left);
+	righ_t += height(tree->right);
+	return (lef_t  - righ_t);
+}
+/**
+ * height - function promptly calculates the height of the tree
+ * @tree: pointer to the root node
+ *
+ * Return: height of the tree;
+ */
+int height(const binary_tree_t *tree)
+{
 	int lef_t, righ_t;
 
 	if (tree == NULL)
 		return (-1);
-	lef_t = binary_tree_balance(tree->left);
-	righ_t = binary_tree_balance(tree->right);
-	return (lef_t  - righ_t);
+	lef_t = height(tree->left);
+	righ_t = height(tree->right);
+	if (lef_t > righ_t)
+		return (lef_t + 1);
+	else
+		return (righ_t + 1);
 }
-
